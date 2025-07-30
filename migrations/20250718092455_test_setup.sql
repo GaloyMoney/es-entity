dae3f96a4db09d5836e3f3c8d9d7a3ba1dca9a25
Lakshyyaa <lakshh113@gmail.com>
2025-07-30 19:59:30 +0530

    chore: remove unused table

--- migrations/20250718092455_test_setup.sql
+++ migrations/20250718092455_test_setup.sql
@@ -45,19 +45,3 @@ CREATE TABLE custom_name_for_user_events (
   recorded_at TIMESTAMPTZ NOT NULL,
   UNIQUE(id, sequence)
 );
-
-CREATE TABLE ignore_prefix_custom_name_for_users (
-  id UUID PRIMARY KEY,
-  name VARCHAR NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL
-);
-CREATE INDEX idx_ignore_prefix_custom_name_for_users_name ON ignore_prefix_custom_name_for_users (name);
-
-CREATE TABLE ignore_prefix_custom_name_for_user_events (
-  id UUID NOT NULL REFERENCES ignore_prefix_custom_name_for_users(id),
-  sequence INT NOT NULL,
-  event_type VARCHAR NOT NULL,
-  event JSONB NOT NULL,
-  recorded_at TIMESTAMPTZ NOT NULL,
-  UNIQUE(id, sequence)
-);
