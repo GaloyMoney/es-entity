20affea8dc8d0adecf660827e9e6a7bfee372dd7
bodymindarts <justin@galoy.io>
2025-08-07 11:58:43 +0200

    ci: version stuff

--- ci/tasks/set-dev-version.sh
+++ ci/tasks/set-dev-version.sh
@@ -4,25 +4,12 @@ VERSION="$(cat version/version)-dev"
 
 pushd repo
 
-for file in $(find . -mindepth 2 -name Cargo.toml); do
+for file in $(find . -name Cargo.toml); do
     sed -i'' "s/^version.*/version = \"${VERSION}\"/" ${file}
 done
 
-sed -i'' "s/cel-parser\", version = .*/cel-parser\", version = \"${VERSION}\" }/" cala-cel-interpreter/Cargo.toml
-sed -i'' "s/cel-interpreter\", version = .*/cel-interpreter\", version = \"${VERSION}\" }/" cala-ledger-core-types/Cargo.toml
-sed -i'' "s/core-types\", version = .*/core-types\", version = \"${VERSION}\" }/" cala-ledger-outbox-client/Cargo.toml
-sed -i'' "s/cel-interpreter\", version = .*/cel-interpreter\", version = \"${VERSION}\" }/" cala-ledger-outbox-client/Cargo.toml
-
-
-sed -i'' "s/core-types\", version = .*/core-types\", version = \"${VERSION}\" }/" cala-ledger/Cargo.toml
-sed -i'' "s/cel-interpreter\", version = .*/cel-interpreter\", version = \"${VERSION}\" }/" cala-ledger/Cargo.toml
-sed -i'' "s/cala-tracing\", version = .*/cala-tracing\", version = \"${VERSION}\" }/" cala-ledger/Cargo.toml
-
-sed -i'' "s/core-types\", version = .*/core-types\", version = \"${VERSION}\" }/" cala-server/Cargo.toml
-sed -i'' "s/cala-ledger\", version = .*/cala-ledger\", version = \"${VERSION}\" }/" cala-server/Cargo.toml
-sed -i'' "s/cel-interpreter\", version = .*/cel-interpreter\", version = \"${VERSION}\" }/" cala-server/Cargo.toml
-sed -i'' "s/cala-ledger-outbox-client\", version = .*/cala-ledger-outbox-client\", version = \"${VERSION}\" }/" cala-server/Cargo.toml
-sed -i'' "s/cala-tracing\", version = .*/cala-tracing\", version = \"${VERSION}\" }/" cala-server/Cargo.toml
+sed -i'' "s/sim-time\", version = .*/sim-time\", version = \"${VERSION}\" }/" ./Cargo.toml
+sed -i'' "s/es-entity-macros\", version = .*/es-entity-macros\", version = \"${VERSION}\" }/" ./Cargo.toml
 
 cargo update --workspace
 
