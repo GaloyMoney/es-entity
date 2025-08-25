d1f73c17afe5fdaece0a7ff4ab9d1d6583c9c869
bodymindarts <justin@galoy.io>
2025-08-25 14:38:30 +0200

    refactor: remove Arc

--- src/context.rs
+++ src/context.rs
@@ -1,17 +1,16 @@
 use im::HashMap;
 use serde::Serialize;
 
-use std::{cell::RefCell, sync::Arc};
+use std::cell::RefCell;
 
-type ContextData = Arc<HashMap<&'static str, serde_json::Value>>;
+type ContextData = HashMap<&'static str, serde_json::Value>;
 
 thread_local! {
     static CONTEXT_STACK: RefCell<Vec<ContextData>> = RefCell::new(Vec::new());
 }
 
 pub struct EventContext {
-    // Marker to track when to pop from stack
-    _marker: (),
+    data: ContextData,
 }
 
 impl Drop for EventContext {
@@ -27,16 +26,17 @@ impl Drop for EventContext {
 
 impl EventContext {
     pub fn current() -> Self {
-        CONTEXT_STACK.with(|c| {
+        let data = CONTEXT_STACK.with(|c| {
             let mut stack = c.borrow_mut();
             if stack.is_empty() {
-                stack.push(Arc::new(HashMap::new()));
+                stack.push(HashMap::new());
             } else {
-                let current_data = Arc::clone(stack.last().unwrap());
+                let current_data = stack.last().unwrap().clone();
                 stack.push(current_data);
             }
+            stack.last().unwrap().clone()
         });
-        EventContext { _marker: () }
+        EventContext { data }
     }
 
     pub fn fork() -> Self {
@@ -49,29 +49,23 @@ impl EventContext {
         value: &T,
     ) -> Result<(), serde_json::Error> {
         let json_value = serde_json::to_value(value)?;
+        self.data = self.data.update(key, json_value);
+
         CONTEXT_STACK.with(|c| {
             let mut stack = c.borrow_mut();
             if let Some(last) = stack.last_mut() {
-                let new_data = last.update(key, json_value);
-                *last = Arc::new(new_data);
+                *last = self.data.clone();
             }
         });
         Ok(())
     }
 
     pub fn as_json(&self) -> Result<serde_json::Value, serde_json::Error> {
-        CONTEXT_STACK.with(|c| {
-            let stack = c.borrow();
-            if let Some(data) = stack.last() {
-                let mut map = serde_json::Map::new();
-                for (k, v) in data.iter() {
-                    map.insert(k.to_string(), v.clone());
-                }
-                Ok(serde_json::Value::Object(map))
-            } else {
-                Ok(serde_json::Value::Object(serde_json::Map::new()))
-            }
-        })
+        let mut map = serde_json::Map::new();
+        for (k, v) in self.data.iter() {
+            map.insert(k.to_string(), v.clone());
+        }
+        Ok(serde_json::Value::Object(map))
     }
 }
 
