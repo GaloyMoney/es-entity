4b03c3a1110861a4f4cd494c509f85e853998bd7
Sandipan <sandipandey.12@gmail.com>
2025-09-16 23:45:10 +0530

    ci: fix release script (#48)

--- ci/vendor/tasks/prep-release-src.sh
+++ ci/vendor/tasks/prep-release-src.sh
@@ -35,16 +35,20 @@ echo -n "Prev Version: "
 cat version/version
 echo ""
 
-# Initial Version
 if [[ $(cat version/version) == "0.0.0" ]]; then
   echo "0.1.0" > version/version
-# Figure out proper version to release
-elif [[ $(cat artifacts/gh-release-notes.md | grep breaking) != '' ]] || [[ $(cat artifacts/gh-release-notes.md | grep feature) != '' ]]; then
-  echo "Breaking change / Feature Addition found, bumping minor version..."
-  bump2version minor --current-version $(cat version/version) --allow-dirty version/version
+
+elif grep -q '\[**breaking**\]' artifacts/gh-release-notes.md; then
+  echo "Breaking change found, bumping minor version..."
+  bump2version minor --current-version "$(cat version/version)" --allow-dirty version/version
+
+elif grep -q '^### Features' artifacts/gh-release-notes.md; then
+  echo "Feature section found, bumping minor version..."
+  bump2version minor --current-version "$(cat version/version)" --allow-dirty version/version
+
 else
-  echo "Only patches and fixes found - no breaking changes, bumping patch version..."
-  bump2version patch --current-version $(cat version/version) --allow-dirty version/version
+  echo "Only patches and fixes found - bumping patch version..."
+  bump2version patch --current-version "$(cat version/version)" --allow-dirty version/version
 fi
 
 echo -n "Release Version: "
